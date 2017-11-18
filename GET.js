3b4c4fde83dd8b4c516e9215a7b3b838907b86d0
hocktoh89 <yhtthomas@gmail.com>
2017-11-18 17:00:56 +0800

    set urlShortener

--- GET.js
+++ GET.js
@@ -1,4 +1,16 @@
-exports.getShortenUrl = function(req, res) {
-    
-    res.send('Hey Thomas');
+import fetch from 'node-fetch';
+
+const access_token = 'f104c6ae7ba6cc03814cac9fa1705569a1b82b72';
+const longUrl = 'https://www.khanacademy.org/computing/computer-science/informationtheory/info-theory/v/language-of-coins-2-8-proto-writing';
+
+exports.getShortenUrl = async function(req, res) {
+    let result = {};
+    await fetch(`https://api-ssl.bitly.com/v3/shorten?access_token=${access_token}&longUrl=${longUrl}`)
+    .then(function(res) {
+        return res.json();
+    }).then(function(json) {
+       result = json;
+    });
+
+    res.send(`Hey Thomas ${result.data.url}`);
 };
